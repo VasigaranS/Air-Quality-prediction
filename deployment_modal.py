030d1cb2169d1fb1dd029ce965f67ca6f8b8fae9
Vasigaran Senthilkumar <44389565+VasigaranS@users.noreply.github.com>
2023-01-14 23:07:20 +0100

    Add files via upload

---
+++ deployment_modal.py
@@ -0,0 +1,251 @@
+import os
+import modal
+import pandas as pd
+from datetime import datetime
+import time 
+import requests
+import hopsworks
+from datetime import timedelta, datetime
+LOCAL=False
+AIR_QUALITY_API_KEY="722121a6290f3209a19231fcbd93baf2209fbe52"
+WEATHER_API_KEY="UNCQW6ZMAX2NNBT5LJSPRAAL5"
+
+if LOCAL == False:
+   stub = modal.Stub()
+   image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4"]) 
+
+   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+   def f():
+       g()
+
+
+
+
+def g():
+    
+    date_today = datetime.now()
+    date_today =date_today.strftime("%Y-%m-%d")
+    cities = ['Amsterdam']
+
+
+    data_air_quality = [get_air_quality_data(city) for city in cities]
+
+    data_weather = [get_weather_data(city, date_today) for city in cities]
+
+
+    df_air_quality = get_air_quality_df(data_air_quality)
+    df_weather = get_weather_df(data_weather)
+    
+    aqi=[]
+    for i in range(0,len(df_air_quality)):
+        row=df_air_quality.iloc[i,1:-2]
+        aqi.append(max(row))
+    df_air_quality['aqi']=aqi
+    df_air_quality['aqi'] = df_air_quality['aqi'].astype(int)
+
+    project = hopsworks.login()
+
+    fs = project.get_feature_store() 
+
+    air_quality_fg = fs.get_or_create_feature_group(
+        name = 'air_quality_fg',
+        version = 1
+    )
+    weather_fg = fs.get_or_create_feature_group(
+        name = 'weather_fg',
+        version = 1
+    )
+
+    air_quality_fg.insert(df_air_quality)
+    weather_fg.insert(df_weather)
+
+
+
+def decode_features(df, feature_view):
+    """Decodes features in the input DataFrame using corresponding Hopsworks Feature Store transformation functions"""
+    df_res = df.copy()
+
+    import inspect
+
+
+    td_transformation_functions = feature_view._batch_scoring_server._transformation_functions
+
+    res = {}
+    for feature_name in td_transformation_functions:
+        if feature_name in df_res.columns:
+            td_transformation_function = td_transformation_functions[feature_name]
+            sig, foobar_locals = inspect.signature(td_transformation_function.transformation_fn), locals()
+            param_dict = dict([(param.name, param.default) for param in sig.parameters.values() if param.default != inspect._empty])
+            if td_transformation_function.name == "min_max_scaler":
+                df_res[feature_name] = df_res[feature_name].map(
+                    lambda x: x * (param_dict["max_value"] - param_dict["min_value"]) + param_dict["min_value"])
+
+            elif td_transformation_function.name == "standard_scaler":
+                df_res[feature_name] = df_res[feature_name].map(
+                    lambda x: x * param_dict['std_dev'] + param_dict["mean"])
+            elif td_transformation_function.name == "label_encoder":
+                dictionary = param_dict['value_to_index']
+                dictionary_ = {v: k for k, v in dictionary.items()}
+                df_res[feature_name] = df_res[feature_name].map(
+                    lambda x: dictionary_[x])
+    return df_res
+
+
+def get_model(project, model_name, evaluation_metric, sort_metrics_by):
+    """Retrieve desired model or download it from the Hopsworks Model Registry.
+
+    In second case, it will be physically downloaded to this directory"""
+    TARGET_FILE = "model.pkl"
+    list_of_files = [os.path.join(dirpath,filename) for dirpath, _, filenames \
+                     in os.walk('.') for filename in filenames if filename == TARGET_FILE]
+
+    if list_of_files:
+        model_path = list_of_files[0]
+        model = joblib.load(model_path)
+    else:
+        if not os.path.exists(TARGET_FILE):
+            mr = project.get_model_registry()
+            # get best model based on custom metrics
+            model = mr.get_best_model(model_name,
+                                      evaluation_metric,
+                                      sort_metrics_by)
+            model_dir = model.download()
+            model = joblib.load(model_dir + "/model.pkl")
+
+    return model
+
+
+def get_air_json(city_name, AIR_QUALITY_API_KEY):
+    return requests.get(f'https://api.waqi.info/feed/{city_name}/?token={AIR_QUALITY_API_KEY}').json()['data']
+
+
+def get_air_quality_data(city_name):
+    #AIR_QUALITY_API_KEY = os.getenv('AIR_QUALITY_API_KEY')
+    json = get_air_json(city_name, AIR_QUALITY_API_KEY)
+    
+    iaqi = json['iaqi']
+    forecast = json['forecast']['daily']
+    return [
+        
+                         
+        json['time']['s'][:10],      # Date
+        iaqi['pm25']['v'],
+        iaqi['pm10']['v'],
+        iaqi['o3']['v'],
+        iaqi['no2']['v'],
+        iaqi['so2']['v'],
+        iaqi['co']['v'],
+        json['aqi']# AQI
+        
+    ]
+
+def get_air_quality_df(data):
+    col_names = [      
+        'date',
+        'pm25',
+        'pm10',
+        'o3',
+        'no2',
+        'so2',
+        'co',
+        'aqi'
+    ]
+
+    new_data = pd.DataFrame(
+        data,
+        columns=col_names
+    )
+    new_data.date = new_data.date.apply(timestamp_2_time_weather)
+
+    return new_data
+
+
+def get_weather_json(city, date, WEATHER_API_KEY):
+    return requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city.lower()}/{date}?unitGroup=metric&include=days&key={WEATHER_API_KEY}&contentType=json').json()
+
+
+def get_weather_data(city_name, date):
+    #WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+    json = get_weather_json(city_name, date, WEATHER_API_KEY)
+    data = json['days'][0]
+
+
+    return [
+        
+        data['datetime'],
+        data['tempmax'],
+        data['tempmin'],
+        data['temp'],
+        data['feelslikemax'],
+        data['feelslikemin'],
+        data['feelslike'],
+        data['dew'],
+        data['humidity'],
+        data['precip'],
+        data['precipprob'],
+        data['precipcover'],
+
+        data['windgust'],
+        data['windspeed'],
+        data['winddir'],
+        data['pressure'],
+        data['cloudcover'],
+        data['visibility'],
+        data['solarradiation'],
+        data['solarenergy'],
+        data['uvindex'],
+        data['conditions']
+    ]
+
+
+def get_weather_df(data):
+    col_names = [
+        'date',
+        'tempmax',
+        'tempmin',
+        'temp',
+        'feelslikemax',
+        'feelslikemin',
+        'feelslike',
+        'dew',
+        'humidity',
+        'precip',
+        'precipprob',
+        'precipcover',
+        
+        'windgust',
+        'windspeed',
+        'winddir',
+        'sealevelpressure',
+        'cloudcover',
+        'visibility',
+        'solarradiation',
+        'solarenergy',
+        'uvindex',
+        'conditions'
+    ]
+
+    new_data = pd.DataFrame(
+        data,
+        columns=col_names
+    )
+    new_data.date = new_data.date.apply(timestamp_2_time_weather)
+
+    return new_data
+
+def timestamp_2_time(x):
+    dt_obj = datetime.strptime(str(x), '%Y/%m/%d')
+    dt_obj = dt_obj.timestamp() * 1000
+    return int(dt_obj)
+
+def timestamp_2_time_weather(x):
+    dt_obj = datetime.strptime(str(x), '%Y-%m-%d')
+    dt_obj = dt_obj.timestamp() * 1000
+    return int(dt_obj)
+
+if __name__ == "__main__":
+    if LOCAL == True :
+        g()
+    else:
+         stub.deploy("f")
+         #   f()
